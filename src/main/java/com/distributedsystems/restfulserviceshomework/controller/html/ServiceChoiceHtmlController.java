4141e31323ba60067954c635620a81d72b790497
jakubowiczish <jakub77plotnikowski@gmail.com>
2020-03-11 22:57:39 +0100

    Change default mapping to services list.

--- src/main/java/com/distributedsystems/restfulserviceshomework/controller/html/ServiceChoiceHtmlController.java
+++ src/main/java/com/distributedsystems/restfulserviceshomework/controller/html/ServiceChoiceHtmlController.java
@@ -3,7 +3,6 @@ package com.distributedsystems.restfulserviceshomework.controller.html;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class ServiceChoiceHtmlController {
 
-    @GetMapping("/services")
+    @RequestMapping("/")
     public String servicesList() {
         return "services_list";
     }
